ae3a026d1aa5fda7be5c332ed9726bd8c49bd264
Patrick Cook <patrick.cook@disney.com>
2012-09-17 14:42:03 -0700

    Modified to read text of selected option instead of HTML

--- jquery/js/jquery.combobox.js
+++ jquery/js/jquery.combobox.js
@@ -183,7 +183,7 @@
             var thisSelector = this;
             this.selectorElement.find('li').click(function (e) {
                 thisSelector.hide();
-                thisSelector.combobox.setValue(this.innerHTML);
+                thisSelector.combobox.setValue(jQuery(this).text());
                 thisSelector.combobox.focus();
             });
             this.selectorElement.mouseover(function (e) {
@@ -250,7 +250,7 @@
         
         getSelectedValue : function () {
             if(this.selectedIndex !== -1){
-                return this.selectorElement.find('li').get(this.selectedIndex).innerHTML;
+                return jQuery(this.selectorElement.find('li').get(this.selectedIndex)).text();
             } else {
                 return this.combobox.textInputElement.val();
             }
